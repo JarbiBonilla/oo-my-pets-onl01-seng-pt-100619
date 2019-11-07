043b8b766d043d4bf66fb4fb40b57b2e5b4e28dd
Jarbi Bonilla <jarbibonilla@gmail.com>
2019-11-07 03:19:12 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,13 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+  end
+  
+  def say_species
+    self 
+  end
+  
 end
